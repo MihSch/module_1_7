39cbfc603903156146bc33d0eaff708b5d6561f6
MihSch <moco4ino@gmail.com>
2024-09-02 18:23:11 +0400

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,16 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = ['Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron']
+res = students.sort()
+students.sort ()
+average = sum (grades[0])/len (grades[0])
+average1 = sum (grades[1])/len (grades[1])
+average2 = sum (grades[2])/len (grades[2])
+average3 = sum (grades[3])/len (grades[3])
+average4 = sum (grades[4])/len (grades[4])
+print (students[0] , ":", average,
+students[1] , ":", average1,
+students[2] , ":", average2,
+students[3] , ":", average3,
+students[4] , ":", average4
+
+)
